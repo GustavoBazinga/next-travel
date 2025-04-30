818acd9b8d5b525b1c1185099df4f18780c20f71
user <al.gustavo@outlook.com>
2025-04-29 22:14:54 -0300

    base

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>Sobre o Next Travel</h1>
+      <p>Um site de viagens feito com Next.js</p>
+      <p>Esta é a página sobre o nosso site de viagens.</p>
+    </main>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,17 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <ul className="menu">
+                <li>
+                <a href="/">Home</a>
+                </li>
+                <li>
+                <a href="/about">Sobre</a>
+                </li>
+                <li>
+                <a href="/destinos">Destinos</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
